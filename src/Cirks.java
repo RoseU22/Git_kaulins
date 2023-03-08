f6d5de062d01c0cebd4dedaeef2a081430753bdc
Roze <rozeu668@gmail.com>
2023-03-08 09:39:31 +0200

    Ieviesta iesp'eja mest kauliņu vairākas reizes

--- src/Cirks.java
+++ src/Cirks.java
@@ -2,14 +2,16 @@ import java.util.Random;
 
 public class Cirks {
 
-	static void mestKaulinu(){
+	static void mestKaulinu(int reizes){
 		int skaitlis;
 		Random rand = new Random();
+		for(int i=1; i<=reizes; i++){
 		skaitlis = rand.nextInt(6)+1;
-		System.out.println("UZkrita skaitlis: "+skaitlis);
+		System.out.println("Uzkrita skaitlis: "+skaitlis);
+		}
 	}
 	
 	public static void main(String[] args) {
-		mestKaulinu();
+		mestKaulinu(3);
 	}
 }
