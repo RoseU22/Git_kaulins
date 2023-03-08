2c9d6dea06585eade7e9751540e284a4a09c2344
Roze <rozeu668@gmail.com>
2023-03-08 09:48:53 +0200

    Integrēta iespēja no konsoles ievadīt metienu skaitu

--- src/Cirks.java
+++ src/Cirks.java
@@ -1,16 +1,23 @@
 import java.util.Random;
+import java.util.Scanner;
 
 public class Cirks {
 
 	public static void main(String[] args) {
 		
-		int skaitlis;
+		int skaitlis, reizes;
+		
 		Random rand = new Random();
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.println("Cik reizes mest kauliòu?");
+		reizes = scan.nextInt();
+		
+		for(int i=1; i<=reizes; i++){
 		skaitlis = rand.nextInt(6)+1;
 		System.out.println("Uzkrita skaitlis: "+skaitlis);
-		
-		
-		
+	
+		}
+		scan.close();
 	}
-
 }
