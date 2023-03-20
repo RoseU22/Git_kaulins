68d6c50ace1293083f733e780fa3cb1c36d4af8c
Roze <rozeu668@gmail.com>
2023-03-20 16:03:11 +0200

    Lokālajā Rzarā papildināta programma

--- src/Cirks.java
+++ src/Cirks.java
@@ -4,12 +4,16 @@ import java.util.Scanner;
 public class Cirks {
 	
 	static void mestKaulinu(int reizes){
-		int skaitlis;
+		int skaitlis, lielakais=0;
 		Random rand = new Random();
 		for(int i=1; i<=reizes; i++){
 		skaitlis = rand.nextInt(6)+1;
 		System.out.println("Uzkrita skaitlis: "+skaitlis);
+		
+		if(lielakais<skaitlis)
+			lielakais = skaitlis;
 		}
+		System.out.println("Lielâkais skaitlis metienos ir "+lielakais);
 	}	
 	
 	public static void main(String[] args) {
@@ -22,6 +26,7 @@ public class Cirks {
 		System.out.println("Cik reizes mest kauliòu?");
 		reizes = scan.nextInt();
 		}while(reizes<1);
+		
 		mestKaulinu(reizes);
 		scan.close();
 	}
